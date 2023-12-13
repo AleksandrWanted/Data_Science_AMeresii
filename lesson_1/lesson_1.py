e3741c47ebd0faba810e827bd35eea635c5a98d9
a.meresiy <a.meresiy@postgrespro.ru>
2023-12-13 22:33:38 +0500

    add lesson_1

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+"""Задание 1:
+
+Используя офисное приложение или текстовый редактор
+подготовить данные в формате .csv
+
+Ответ: файл rand_matrix.csv
+
+"""
+
+
+"""Задание 2:
+
+Прочитать эти данные в numpy
+
+"""
+
+matrix = np.loadtxt(delimiter=",", fname='lesson_1\\rand_matrix.csv')
+print('Матрица полученная из файла rand_matrix.csv:\n%s' % matrix)
+
+
+"""Задание 3:
+
+Найти максимум и минимум и поменять их местами
+
+"""
+
+matrix_max = np.max(matrix)
+matrix_min = np.min(matrix)
+
+print("максимум - %s" % matrix_max)
+print("минимум - %s" % matrix_min)
+
+r_max = np.where(matrix == matrix_max)
+r_min = np.where(matrix == matrix_min)
+
+matrix[r_max[0], r_max[1]] = matrix_min
+matrix[r_min[0], r_min[1]] = matrix_max
+
+print('Матрица после замены max<>min:\n%s' % matrix)
